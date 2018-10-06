24cdf68a04a60615fa95df734c0c4113a490ae50
Matthew <nokturnusmf@gmail.com>
2018-10-06 14:34:35 +0100

    Added support for multiple input files to bta

--- tools/bmp_to_arr.cpp
+++ tools/bmp_to_arr.cpp
@@ -2,64 +2,86 @@
 #include <fstream>
 #include <iomanip>
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <infile> <outfile>\n";
-        return 1;
-    }
+struct Image {
+    unsigned char* pixels;
+    int h;
+    int w;
+};
 
-    std::ifstream infile(argv[1], std::ios::binary);
-    if (!infile) {
-        std::cerr << "Couldn't open infile\n";
-        return 2;
-    }
-
-    std::ofstream outfile(argv[2]);
-    if (!outfile) {
-        std::cerr << "Couldn't open outfile\n";
-    }
-    outfile.flags(std::ios::hex);
-
+Image read(std::ifstream& file) {
     int skip;
-    infile.seekg(10, std::ios::beg);
-    infile.read((char*)&skip, 4);
+    file.seekg(10, std::ios::beg);
+    file.read((char*)&skip, 4);
 
     int h, w;
-    infile.seekg(18, std::ios::beg);
-    infile.read((char*)&w, 4);
-    infile.read((char*)&h, 4);
+    file.seekg(18, std::ios::beg);
+    file.read((char*)&w, 4);
+    file.read((char*)&h, 4);
     int size = h * w * 3;
 
-    infile.seekg(skip, std::ios::beg);
+    file.seekg(skip, std::ios::beg);
 
     unsigned char* data = new unsigned char[size];
-    infile.read((char*)data, size);
-    infile.close();
+    file.read((char*)data, size);
+    
+    return { data, h, w };
+}
 
+unsigned short convert(unsigned char r, unsigned char g, unsigned char b) {
+    unsigned short val = 0;
+
+    val |= r >= 248 ? 31 : (r + 4) >> 3;
+    val <<= 6;
+
+    val |= g >= 252 ? 63 : (g + 2) >> 2;
+    val <<= 5;
+
+    val |= b >= 248 ? 31 : (b + 4) >> 3;
+    
+    return val;
+}
+
+void output(std::ofstream& file, Image& img) {
     int k = 0;
-    for (int i = (h - 1) * w * 3; i >= 0; i -= w * 3) {
-        for (int j = 0; j < w * 3; j += 3) {
-            unsigned short val = 0;
-
-            auto b = data[i + j + 2];
-            val |= b >= 248 ? 31 : (b + 4) >> 3;
-            val <<= 6;
-
-            b = data[i + j + 1];
-            val |= b >= 252 ? 63 : (b + 2) >> 2;
-            val <<= 5;
-
-            b = data[i + j];
-            val |= b >= 248 ? 31 : (b + 4) >> 3;
-
-            outfile << "0x" << std::setfill('0') << std::setw(4) << val << ", ";
+    for (int i = (img.h - 1) * img.w * 3; i >= 0; i -= img.w * 3) {
+        for (int j = 0; j < img.w * 3; j += 3) {
+            file << "0x" << std::setfill('0') << std::setw(4) << convert(img.pixels[i + j + 2], img.pixels[i + j + 1], img.pixels[i + j]) << ", ";
 
             if (++k % 8 == 0) {
-                outfile << '\n';
+                file << '\n';
             }
         }
     }
-    outfile.close();
+}
 
-    delete[] data;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <infile1> (infile...) <outfile>\n";
+        return 1;
+    }
+
+    std::ofstream outfile(argv[argc - 1]);
+    if (!outfile) {
+        std::cerr << "Couldn't open outfile\n";
+        return 2;
+    }
+    outfile.flags(std::ios::hex);
+    
+    for (int i = 1; i < argc - 1; ++i) {
+        std::ifstream infile(argv[i], std::ios::binary);
+        if (!infile) {
+            std::cerr << "Couldn't open infile " << argv[i] << '\n';
+            outfile.close();
+            return 3;
+        }
+        
+        auto img = read(infile);
+        infile.close();
+        
+        output(outfile, img);
+        
+        delete[] img.pixels;
+    }
+    
+    outfile.close();
 }
