6eaaef1fe1e528ea04ce3a5c51405d4abe2b9596
Matthew <nokturnusmf@gmail.com>
2018-12-25 17:32:26 +0000

    Refactored Makefiles

--- bird/src/renderer.h
+++ bird/src/renderer.h
@@ -1,15 +1,1 @@
-#ifndef RENDERER_H
-#define RENDERER_H
-
-#include <fxcg/display.h>
-
-void init_renderer();
-
-void clear(unsigned short bg = 0xffff, int start_row = 0, int end_row = LCD_HEIGHT_PX);
-
-void draw_sprite(const unsigned short* sprite, int x, int y, int w, int h);
-
-void write_frame();
-
-#endif // RENDERER_H
-
+../../lib/utils/renderer.h
