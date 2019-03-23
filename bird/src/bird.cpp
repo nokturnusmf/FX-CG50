823b660b0c9b4a729cc1c234c393a013e35504a1
Matthew Foulds <nokturnusmf@gmail.com>
2019-03-23 17:37:30 +0000

    Added high score saving

--- bird/src/bird.cpp
+++ bird/src/bird.cpp
@@ -1,6 +1,7 @@
 #include <fxcg/system.h>
 #include <fxcg/display.h>
 #include <fxcg/keyboard.h>
+#include <fxcg/file.h>
 
 #include <string.h>
 
@@ -8,30 +9,49 @@
 #include "game.h"
 #include "util.h"
 
+#define UC_LIT(x) (reinterpret_cast<unsigned char*>(const_cast<char*>(x)))
+
+void display(int score, int high) {
+    MsgBoxPush(2);
+
+    char score_str[20] = "  Score: ";
+    char high_str[20] = "  High: ";
+
+    itoa(score, score_str + 9);
+    itoa(high, high_str + 8);
+
+    PrintXY(6, 3, score_str, 0, TEXT_COLOR_BLACK);
+    PrintXY(7, 4, high_str, 0, TEXT_COLOR_BLACK);
+
+    int t;
+    GetKey(&t);
+    MsgBoxPop();
+}
+
+static int high = 0;
+
+void quit_handler() {
+    MCSDelVar2(UC_LIT("BIRD"), UC_LIT("SCORE"));
+    MCSPutVar2(UC_LIT("BIRD"), UC_LIT("SCORE"), 4, &high);
+}
+
 int main() {
+    int len;
+    MCS_CreateDirectory(UC_LIT("BIRD"));
+    MCSGetDlen2(UC_LIT("BIRD"), UC_LIT("SCORE"), &len);
+    MCSGetData1(0, 4, &high);
+
+    SetQuitHandler(quit_handler);
+
     EnableStatusArea(3);
     init_renderer();
 
-    int high = 0;
     while (1) {
         int score = game();
         if (score > high) {
             high = score;
         }
 
-        MsgBoxPush(2);
-
-        char score_str[20] = "  Score: ";
-        char high_str[20] = "  High: ";
-
-        itoa(score, score_str + 9);
-        itoa(high, high_str + 8);
-
-        PrintXY(6, 3, score_str, 0, TEXT_COLOR_BLACK);
-        PrintXY(7, 4, high_str, 0, TEXT_COLOR_BLACK);
-
-        int t;
-        GetKey(&t);
-        MsgBoxPop();
+        display(score, high);
     }
 }
